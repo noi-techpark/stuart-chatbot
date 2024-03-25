b65aeb16e89724d7724f6fb58ebbb0704404b86c
Chris Mair <chris@1006.org>
2024-03-25 19:42:24 +0100

    expanded this to a full-fledged interactive chat application

--- rag/query.py
+++ rag/query.py
@@ -1,8 +1,9 @@
 # ------------------------------------------------------------------------------
-# Accept user input as a single argument from the command line.
-# Find and list top-matching chunks via vector search. Construct
-# an LLM context using this information and run the LLM.
-# Output the result.
+# Interactive chat application.
+# In a loop, prompt for user input and perform vector search, prompt
+# the LLM using the user input and the top matching result from the
+# search. In further iterations, include the LLM output in the search.
+# Type 'q' to exit or 'r' to reset the conversation (forget the context).
 #
 # Note:
 #
@@ -21,10 +22,17 @@
 #
 #     ref. original model: https://huggingface.co/mistralai/Mistral-7B-Instruct-v0.2
 #     ref. quantized versions: https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF
+#
+# Some sample questions:
+#
+#   Wie kann ich ein Hotel als familienfreundlich taggen?
+#   Come posso ottenere informazioni sui mercatini di Natale di Bolzano?
+#   Who do I need to contact to obtain a user account for making authenticated requests to the Open Data Hub?
 # ------------------------------------------------------------------------------
 
 import time
 import sys
+import re
 from pprint import pprint
 
 from librag import *
@@ -32,119 +40,135 @@ from libpg import *
 
 from llama_cpp import Llama
 
-# --- user input ---
-
-# some sample/default questions
-# question = "Wie kann ich ein Hotel als familienfreundlich taggen?"
-question = "Come posso ottenere informazioni sui mercatini di Natale di Bolzano?"
-# question = "Who do I need to contact to obtain a user account for making authenticated requests to the Open Data Hub?"
-if len(sys.argv) >= 2:
-    question = sys.argv[1]
-print("---")
-print("user input:")
-print(question)
-
-# --- search ---
-
-cursor = open_cursor()
-top = 10
-res = search(cursor, top, question)
-
-print("---")
-print("embedding vector search: top %d chunks:" % top)
-print("distance tag   offset  file_name")
-for r in res:
-    print("%.4f %6s %7d  %s" % (r["distance"], r["tag"], r["start_pos"], r["file_name"]))
-
-if len(res) < top:
-    print("ERROR: not enough results.")
-    sys.exit(1)
-
 # --- load LLM ---
 
 llm = Llama(
-      model_path="../../mistral-7b-instruct-v0.2.Q5_K_M.gguf",
-      chat_format="mistral-instruct",
-      n_ctx=8192,
-      n_gpu_layers=-1,  # -1 use all available GPU cores, or 0 run on CPU only
-      seed=0,
-      verbose=False
+    model_path="../../mistral-7b-instruct-v0.2.Q5_K_M.gguf",
+    chat_format="mistral-instruct",
+    n_ctx=8192,
+    n_gpu_layers=-1,  # -1 use all available GPU cores, or 0 run on CPU only
+    seed=0,
+    verbose=False
 )
 
-# --- LLM input ---
 
-context = res[0]["file_body"]
+# --- main loop ---
 
-prompt = f"""Answer the question based on the context below.
-If the question can't be answered based on the context, say \"I don't know\".
----
-Context:
-{context}
+while True:
 
----
-Question:
-{question}
-"""
+    iteration = 0
+    last_message = {}
+    conversation = []
 
-conversation = [
-          {"role": "system", "content": "You are an assistant who answers questions."},
-          {"role": "user",   "content": prompt}
-]
+    print("Stuart: You rang 🛎️ ?")
+    print("Ask me anything or enter 'q' to exit. Enter 'r' to restart our conversation.")
 
-print("---")
-print("LLM input:")
-pprint(conversation)
+    while True:
 
-# --- LLM output ---
+        iteration += 1
 
-t0 = time.time()
-result = llm.create_chat_completion(
-      messages=conversation
-)
-t1 = time.time()
+        # --- user input ---
 
-print("---")
-print("LLM output in %6.3fs" % (t1-t0))
-print("usage: %s" % result["usage"])
-print(result["choices"][0].get("message").get("content"))
-print()
 
-# here's how we would refine the answer with the second chunk
-# from the list; this is commented out here as it probably makes
-# more sense in an interactive application where the user also
-# adds something to the discussion
+        question = ""
+        while re.fullmatch(r"\s*", question):
+            question = input("> ")
+            if question in ["q", "Q", "quit", "exit", r"\q"]:
+                sys.exit(0)
 
-# # --- LLM input second iteration ---
-#
-# context = res[1]["file_body"]
-#
-# prompt = f"""We have the opportunity to refine the existing answer
-# (only if needed) with some more context below. Given the additional
-# context, refine the original answer to better answer the query.
-# If the context isn't useful, return the original answer.
-# ---
-# Context:
-# {context}
-# """
-#
-# conversation.append(result["choices"][0].get("message"))
-# conversation.append({"role": "user", "content": prompt})
-#
-# print("---")
-# print("LLM input after refining:")
-# pprint(conversation)
-#
-# # --- LLM output second iteration ---
-#
-# t0 = time.time()
-# result = llm.create_chat_completion(
-#       messages=conversation
-# )
-# t1 = time.time()
-#
-# print("---")
-# print("LLM output after refining in %6.3fs" % (t1-t0))
-# print("usage: %s" % result["usage"])
-# print(result["choices"][0].get("message").get("content"))
-# print()
-#
+        if question in ["r", "R", "reset", r"\r"]:
+            break
+
+        # --- search ---
+
+        cursor = open_cursor()
+        top = 5
+        if iteration == 1:
+            res = search(cursor, top, question)
+        else:
+            res = search(cursor, top, last_message.get("content") + "\n" + question)
+
+        print("{meta}")
+        print("{meta} embedding vector search - top %d chunks:" % top)
+        print("{meta} distance   tag  offset  file_name")
+        print("{meta} --------   ---  ------  ---------")
+        mark = " <-- will be added to context"
+        for r in res:
+            print("{meta} %.5f %6s %7d  %s%s" % (r["distance"], r["tag"], r["start_pos"], r["file_name"], mark))
+            mark = ""
+
+        if len(res) < top:
+            print("ERROR: not enough results.")
+            sys.exit(1)
+
+        print("{meta} please wait...")
+
+        # --- LLM input ---
+
+        context = res[0]["file_body"]
+
+        if iteration == 1:
+
+            # first iteration
+
+            prompt = f"""
+    Answer the question based on the context below.
+    If the question can't be answered based on the context, just say \"I don't know\".
+    ---
+    Context:
+    {context}
+    
+    ---
+    Question:
+    {question}
+    """
+
+            conversation = [
+                      {"role": "system", "content": "You are an assistant who answers questions."},
+                      {"role": "user",   "content": prompt}
+            ]
+
+        else:
+
+            # follow-up iteration
+
+            prompt = f"""
+    {question}
+    
+    Below is more context that might be useful or not.
+    If the context is irrelevant, ignore it silently. Otherwise use it to refine your answer.
+    ---
+    Context:
+    {context}
+    """
+
+            # conversation.append()
+            conversation.append(last_message)
+            conversation.append({"role": "user", "content": prompt})
+
+        # debug print
+        # print("***")
+        # print("LLM input:")
+        # pprint(conversation)
+        # print("***")
+
+        # --- LLM output ---
+
+        t0 = time.time()
+        result = llm.create_chat_completion(
+              messages=conversation
+        )
+        t1 = time.time()
+
+        last_message = result["choices"][0].get("message")
+
+        print("{meta} LLM output in %6.3fs" % (t1-t0))
+        print("{meta} usage: %s" % result["usage"])
+        print("{meta}")
+        print(last_message.get("content"))
+
+        # debug print
+        # print("***")
+        # print("LLM OUTPUT:")
+        # pprint(result)
+        # print("***")
