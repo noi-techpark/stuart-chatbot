45b5d430304252ef23411c1efc30db920f751359
Clemens Zagler <c.zagler@noi.bz.it>
2024-03-27 09:44:57 +0100

    - readme scraper branch priority changed to main/master/development to   reflect most common case and future default - mostly added readme URLs

--- scrapers/scrape_readme.sh
+++ scrapers/scrape_readme.sh
@@ -5,7 +5,7 @@
 # Notes:
 #   - the URLs are hand-picked and read from 'scrape_readme_urls.txt'
 #   - it will get the file from the first branch that exists
-#     in the sequence: development, main, master
+#     in the sequence: main, master, development
 #   - the script tries to download the README.md from each repository
 #     and save it to '../data_readme'
 # ------------------------------------------------------------------------------
@@ -34,7 +34,7 @@ while read -r URL; do
   NAME=$(echo "$URL" | cut -d '/' -f 5)
   NAME="$NAME".md
   SUCCESS=0
-  for BRANCH in development main master; do
+  for BRANCH in main master development; do
     TRYURL=${URL/development/$BRANCH}
     curl -f -L "$TRYURL" > "$NAME" 2>/dev/null
     if [ $? -eq 0 ]; then
